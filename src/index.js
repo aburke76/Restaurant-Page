c079f655e5db9a40b1836caff678bd38b155bcf7
aburke76 <alexlburke8771@gmail.com>
2024-03-29 14:48:09 -0400

    added eventListener for homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+import { homePage } from "./home.js";
+
+const homeBtn = document.querySelector(".home");
+const menuBtn = document.querySelector(".menu");
+const aboutBtn = document.querySelector(".about");
+
+homeBtn.classList.add("active");
+homePage();
+
+homeBtn.addEventListener("click", () => {
+    homeBtn.classList.add("active");
+    homePage();
+});
