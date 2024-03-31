452ed63588b6e6b7a523181afb1353e468a07f36
aburke76 <alexlburke8771@gmail.com>
2024-03-31 15:02:09 -0400

    added loop to remove elements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import { homePage } from "./home.js";
 import { menu } from "./menu.js";
 
+const content = document.querySelector("#content");
 const homeBtn = document.querySelector(".home");
 const menuBtn = document.querySelector(".menu");
 const aboutBtn = document.querySelector(".about");
@@ -10,6 +11,20 @@ homeBtn.setAttribute("id", "active");
 homePage();
 
 homeBtn.addEventListener("click", () => {
+    while (content.lastChild) {
+        content.lastChild.remove();
+    }
+    menuBtn.removeAttribute("id", "active");
     homeBtn.setAttribute("id", "active");
     homePage();
+});
+
+menuBtn.addEventListener("click", () => {
+    while (content.lastChild) {
+        content.lastChild.remove();
+    }
+    homeBtn.disabled = false;
+    homeBtn.removeAttribute("id", "active");
+    menuBtn.setAttribute("id", "active");
+    menu();
 });
